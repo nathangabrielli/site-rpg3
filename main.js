f5466c9602240faea86ca66e8e5ffef74f32b3a4
nathangabrielli <127891969+nathangabrielli@users.noreply.github.com>
2023-03-22 15:56:28 -0300

    mudando a senha

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function logar () {
     var login = document.getElementById('login').value;
     var senha = document.getElementById('senha').value;
     
-    if(login ==  "Haakon" && senha == "1412") {
+    if(login ==  "admin" && senha == "1412") {
        location.href = "aba-pessoal.html";
         return false;
        }
