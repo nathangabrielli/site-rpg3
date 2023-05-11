998b9b41287e3487474b458aae8db8621ebf0e26
nathangabrielli <127891969+nathangabrielli@users.noreply.github.com>
2023-05-11 16:34:01 -0300

    tentando troca a senha pra ver se resolve

--- login.js
+++ login.js
@@ -2,7 +2,7 @@ function logar () {
     var login = document.getElementById('login').value;
     var senha = document.getElementById('senha').value;
     
-    if(login ==  "admin" && senha == "1412") {
+    if(login ==  "admin" && senha == "admin") {
        location.href = "aba-pessoal.html";
         return false;
        }
